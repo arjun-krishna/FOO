f1cc500e057e2bd67bbe79e619dced5eba98809f
arjun-krishna <arjunkrishna.iitm@gmail.com>
2016-09-22 21:28:16 +0530

    initial commit

---
+++ foo.py
@@ -0,0 +1,12 @@
+# Main Foo.py module to call other modules
+
+# Modules To be implemented
+
+# Speech Recognition
+# UI 
+# NLP Part
+# web result ( beautified )
+# Can link to model
+# Virtual Friend ( Chat bot )
+# Speech Synthesis
+
