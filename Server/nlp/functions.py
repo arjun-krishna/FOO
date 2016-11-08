933aaf2dbef4979b716d31dae54edda72bd9621c
suhaschintala <suhaschintala@gmail.com>
2016-11-08 23:23:12 +0530

    scraping basic setup

---
+++ Server/nlp/functions.py
@@ -0,0 +1,6 @@
+def testfunc(a, b):
+	return a
+
+def get_search_string(string):
+	tokens = string.split(' ')
+	return tokens[0]
---
+++ Server/scraper/googlescraper.py
@@ -0,0 +1,25 @@
+def googlescrape(search_string):
+	# do the search
+	# in the top 10 results, choose best stackoverflow/askubuntu/whatever site's answer
+	# Now, scrape the required page for info
+	# get relevant data from the question + answer in the following format,
+	# this is an incomplete version, please add additional relevant info to the object
+	# stackoverflowresult {
+	# 	"question" : String,
+	# 	"answers" : [
+	# 		answerStruct
+	# 	],
+
+	# }
+
+	# answerStruct {
+	# 	desc : "String",
+	# 	upvotes : Integer,
+	# 	is_accepted : Boolean,
+	# 	has_executable_code : Boolean,
+	# 	executable_code : List(String)
+	# 	link  : String
+
+	# }
+	final_dict = {"laugh" : "damn"}
+	return final_dict
